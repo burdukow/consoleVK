dff3ec499a873b633e7701c3211ee22f72c42b78
Shadow-crnn <57222810+Shadow-crnn@users.noreply.github.com>
2020-08-14 22:00:21 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 require_once "functions.php";
 
-shell_exec(clear)
+shell_exec ('clear');
 echo "Enter the VK authorization token \n";
-echo "you can get it by following the link:";
+echo "you can get it by following the link \n";
 echo "https://oauth.vk.com/authorize?client_id=6121396&scope=70658&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1";
 
 $version = file_get_contents("version.txt");
